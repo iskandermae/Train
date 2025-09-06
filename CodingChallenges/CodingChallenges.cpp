746071816580cd4d967f864a7aa5d6aabd58fc8e
iskandermae <to_alexandr@ukr.net>
2025-09-06 19:16:58 +0300

    Update CodingChallenges.cpp

--- CodingChallenges/CodingChallenges.cpp
+++ CodingChallenges/CodingChallenges.cpp
@@ -5,7 +5,7 @@ using namespace std;
 
 int main()
 {
-	int arr[] = { 1, 2 };
+	int arr[] = { 1, 2, 3 };
 	int num = MissingInArray::missingNum(arr, 1);
 	cout << "Missing number is: " << num << endl;
 }
